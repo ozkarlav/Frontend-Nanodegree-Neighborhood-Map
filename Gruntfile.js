1df5123500dfb26724bab424df71b7c4a48880db
ozkarlav <ozkarlav@yahoo.com>
2016-04-15 11:14:01 -0400

    Added Sliding hamburguer menu Got sidebar from https://github.com/BlackrockDigital/startbootstrap-simple-sidebar

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,16 +4,47 @@ module.exports = function(grunt){
 	grunt.initConfig({
 		// Basic settings and info about our plugins
 		pkg: grunt.file.readJSON('package.json'),
-
+		// minify css
 		cssmin: {
-			combine:{
+		  target: {
+		    files: [{
+		      expand: true,
+		      cwd: 'html/css',
+		      src: ['*.css', '!*.min.css'],
+		      dest: 'html/css',
+		      ext: '.min.css'
+		    }]
+		  }
+		},
+		///uglify js
+		uglify: {
+			dist:{
 				files: {
-					'html/css/main.css': ['html/css/stylesheet.css', 'html/css/header.css' ]
+					'html/js/app.min.js': ['html/js/app.js']
 				}
 			}
+		},
+
+		imagemin: {
+		   dist: {
+		      options: {
+		        optimizationLevel: 5
+		      },
+		      files: [{
+		         expand: true,
+		         cwd: 'html/images',
+		         src: ['*.{png,jpg,gif}'],
+		         dest: 'html/dist'
+		      }]
+		   }
 		}
 	});
+	//load the plugin
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.registerTask('default', ['cssmin']);
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+	//Do the task
+    grunt.registerTask('default', ['cssmin', 'uglify']);
 
 };
