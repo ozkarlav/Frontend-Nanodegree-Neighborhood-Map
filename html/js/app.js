69c51a834022fef7bee42c54288413c8c40fee86
ozkarlav <ozkarlav@yahoo.com>
2016-04-21 10:05:35 -0400

    KnockoutJS installed eBird API installed data-binding Birds common name on sidebar

--- html/js/app.js
+++ html/js/app.js
@@ -1,5 +1,11 @@
-// Copyright 2014-2015 Twitter, Inc.
-// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+// var eBirdUrl = "http://ebird.org/ws1.1/data/obs/geo/recent?lng=-81.52&lat=45.19&dist=7&back=8&maxResults=500&locale=en_US&fmt=json";
+//       $.getJSON(eBirdUrl, function (data) {
+//         console.log(data);
+//       }, 
+//       function success(data){
+//         console.log(data);
+//       });
+
 if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
   var msViewportStyle = document.createElement('style')
   msViewportStyle.appendChild(
@@ -9,3 +15,48 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
   )
   document.querySelector('head').appendChild(msViewportStyle)
 };
+// !--------------- VIEW MODEL -----------------!
+
+
+      
+
+function appViewModel() {
+  var eBirdUrl = "http://ebird.org/ws1.1/data/obs/geo/recent?lng=-81.52&lat=45.19&dist=7&back=8&maxResults=25&locale=en_US&fmt=json";
+  var self = this;
+  self.eBirdList = ko.observableArray([]);
+
+  $.getJSON(eBirdUrl, function (data) {
+        self.eBirdList(data);
+        console.log(data);       
+      });
+      
+  // this.eBirdList = ko.observableArrray([]);
+  //console.log(this.eBirdList);
+    
+};
+
+ko.applyBindings(new appViewModel());
+//!---------------- GOOGLE MAP ------------------!
+
+var marker;
+var coordinates = {lat: 45.192551, lng: -81.526823};
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: coordinates
+  });
+
+  marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: coordinates
+  });
+
+  marker.addListener('mouseover', function(){
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  });
+
+  marker.addListener('mouseout', function(){
+    marker.setAnimation(null);
+  });
+}
