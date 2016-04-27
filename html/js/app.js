56c748dcb6fd0ab8214acb14aa161a30d9639032
ozkarlav <ozkarlav@yahoo.com>
2016-04-27 16:04:10 -0400

    Geocode address by searching field set API to search for Bald Eagles near location Markers creation for all eagle points Marker's infoWindow is shows bird's common name and sight date

--- html/js/app.js
+++ html/js/app.js
@@ -15,48 +15,145 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
   )
   document.querySelector('head').appendChild(msViewportStyle)
 };
+
+
+
+
 // !--------------- VIEW MODEL -----------------!
 
 
-      
-
 function appViewModel() {
-  var eBirdUrl = "http://ebird.org/ws1.1/data/obs/geo/recent?lng=-81.52&lat=45.19&dist=7&back=8&maxResults=25&locale=en_US&fmt=json";
   var self = this;
-  self.eBirdList = ko.observableArray([]);
+  var latitude = 42.993826;
+  var longitude = -81.243815;
+  var map;
+  var pos;
+  var infoWindow = null;
+  var userCords;
+  var tempMarkers = [];
+  self.eBirdData = ko.observableArray([]);
+  self.eBirdName = ko.observableArray([]);
+ // self.mapMarkers = ko.observableArray([]);
 
-  $.getJSON(eBirdUrl, function (data) {
-        self.eBirdList(data);
-        console.log(data);       
+  //---eBird API function call
+
+  function geteBirdData () { 
+    self.eBirdData.removeAll();
+    self.eBirdName.removeAll();
+    var eBirdUrl = "http://ebird.org/ws1.1/data/obs/geo_spp/recent?lng="+longitude.toFixed(2)+"&lat="+latitude.toFixed(2)+"&sci=Haliaeetus%20leucocephalus&dist=50&back=5&maxResults=15&locale=en_US&fmt=json&includeProvisional=true"
+    $.getJSON(eBirdUrl, function (data) {
+      var birdPoints = data.length;        
+        for(var i = 0; i< birdPoints; i++){
+            self.eBirdName.push(data[i].locName);
+            self.eBirdData.push({
+              birdName: data[i].comName,
+              birdSights: data[i].howMany,
+              birdLat: data[i].lat,
+              birdLng: data[i].lng,
+              birdLocID: data[i].locID,
+              birdLocName: data[i].locName,
+              birdObsDate: data[i].obsDt,
+              birdSciName: data[i].sciName
+             })
+      }
+      mapMarkers(self.eBirdData());
+      self.eBirdName.sort();
+  })}
+  //
+  function initMap() {
+    //geteBirdData();
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: new google.maps.LatLng (latitude, longitude),
+      panControl: false,
+      panControlOptions: {
+        style: google.maps.ZoomControlStyle.LARGE,
+        position: google.maps.ControlPosition.RIGHT_CENTER
+      },
+      scaleControl: false
+    });
+
+    //Geocode from search on top of map.
+    var geocoder = new google.maps.Geocoder();
+
+    document.getElementById('address').addEventListener('keypress', function(e) {
+      // Enter pressed?
+      if(e.which == 10 || e.which == 13) {
+        geocodeAddress(geocoder, map)}
       });
-      
-  // this.eBirdList = ko.observableArrray([]);
-  //console.log(this.eBirdList);
+  }
+
+    function geocodeAddress(geocoder, resultsMap) {
+        //geteBirdData();
+      var address = document.getElementById('address').value;
+      geocoder.geocode({'address': address}, function(results, status) {
+        self.eBirdData.removeAll();
+        self.eBirdName.removeAll();
+        clearMarkers();
+        mapMarkers(self.eBirdData());
+          if (status === google.maps.GeocoderStatus.OK) {
+            pos = results[0].geometry.location;
+            resultsMap.setCenter(pos);
+            latitude = results[0].geometry.location.lat();
+            longitude = results[0].geometry.location.lng();
+            mapMarkers(self.eBirdData());
+            geteBirdData();
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+      });
+    }
+
+    function mapMarkers(array) {
+      $.each(array, function(index, value) {
+        var latitude = value.birdLat,
+            longitude = value.birdLng,
+            geoLoc = new google.maps.LatLng(latitude, longitude),
+            thisBird = value.birdName,
+            thisDate = value.birdObsDate;
+        var marker = new google.maps.Marker({
+          position: geoLoc,
+          animation: google.maps.Animation.DROP,
+          map: map,
+          title: 'Uluru (Ayers Rock)'
+        });
+        
+        tempMarkers.push(marker);
+        var infowindow = new google.maps.InfoWindow({
+          content: thisBird + ', ' + thisDate,
+        });
+        marker.addListener('click', function() {
+         infowindow.open(map, marker);
+        });
+      });
+        
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0; i < tempMarkers.length; i++) {
+         bounds.extend(tempMarkers[i].getPosition());
+         map.fitBounds(bounds);
+        }
+      }
     
+    function clearMarkers() {
+    for (var i = 0; i < tempMarkers.length; i++ ) {
+      tempMarkers[i].setMap(null);
+    }
+    tempMarkers.length = 0;
+    }
+
+
+   
+  // Create and place markers and info windows on the map based on data from API
+    initMap();
+    mapMarkers(self.eBirdData());
+    geteBirdData();
+  
+
+
 };
 
 ko.applyBindings(new appViewModel());
+
 //!---------------- GOOGLE MAP ------------------!
 
-var marker;
-var coordinates = {lat: 45.192551, lng: -81.526823};
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: coordinates
-  });
 
-  marker = new google.maps.Marker({
-    map: map,
-    animation: google.maps.Animation.DROP,
-    position: coordinates
-  });
-
-  marker.addListener('mouseover', function(){
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  });
-
-  marker.addListener('mouseout', function(){
-    marker.setAnimation(null);
-  });
-}
